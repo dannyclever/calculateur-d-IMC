60bb96455fc6dfa72262fd302df3f88bf3a09e7a
Neto Daniel Zinga <danielnzinga5@gmail.com>
2022-06-26 15:15:06 +0200

    declaration de toutes les variables de la function imc

---
+++ imc.js
@@ -0,0 +1,17 @@
+
+const calculer = document.getElementById('calculer');
+
+
+
+function imc () {
+    const nom = document.getElementById('nom').value;
+    const taille = document.getElementById('taille').value;
+    const poids = document.getElementById('poids').value;
+    const resultat = document.getElementById('resultat');
+
+    if (nom) {
+        
+    } else {
+        
+    }
+}
